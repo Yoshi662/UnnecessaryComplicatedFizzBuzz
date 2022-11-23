4f4a3fd9cee8209247c4b0713bd62c6bd0fe534d
Yoshi <Yoshi66.tm@gmail.com>
2022-11-23 12:36:29 +0100

    Modifier / Added basic functionality about the modifer class

--- UnnecessaryComplicatedFizzBuzz/Model/Modifier.cs
+++ UnnecessaryComplicatedFizzBuzz/Model/Modifier.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace UnnecessaryComplicatedFizzBuzz.Model
 {
-	internal class Modifier
+	public class Modifier
 	{
+		private uint _divisor;
+
+		public uint Divisor
+		{
+			get { return _divisor; }
+			private set { _divisor = value; }
+		}
+
+		private string _output;
+
+		public string Output
+		{
+			get { return _output; }
+			private set { _output = value; }
+		}
+
+		public Modifier(uint divisor, string output)
+		{
+			if (divisor == uint.MinValue) throw new DivideByZeroException("Divisor cannot be zero");
+
+			Divisor = divisor;
+			Output = output;
+		}
+
+		public bool CanModify(uint input) => input % Divisor == uint.MinValue;
+
+		public string Replace(uint input) => CanModify(input) ? Output : input.ToString();
 	}
 }
